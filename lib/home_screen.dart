79e398b90aea3cc0933ca56058f0bb9a68e20f0a
sudoahmed <sudoahmed2001@gmail.com>
2022-01-16 02:44:13 +0500

    Re-added top section

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         body: SingleChildScrollView(
       child: Column(children: const [
+        TopSection(),
         AboutSection(),
         ServiceSection(),
         RecentWorkSection(),
